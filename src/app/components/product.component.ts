1f40979c25786f0563fee185c86ef3bfd351b1ee
Armando Zenil <szenil.11@gmail.com>
2020-05-12 14:03:16 -0500

    Se agrega demostracion del tiempo de ejecucion de los Ciclos de vida de Angular

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,14 +1,34 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, OnInit, DoCheck, OnDestroy } from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('1: Constructor');
+    }
+
+    // ngOnChanges(changes: SimpleChanges) {
+    //     console.log('2: NgOnChanges', changes);
+    // }
+
+    ngOnInit() {
+        console.log('3: NgOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('4. NgDoCheck');
+    }
+
+    ngOnDestroy(){
+        console.log('5. NgOnDestroy');
+    }
 
     addCart() {
         this.productClicked.emit(`Agregado al carrito: ${this.product.id}`);
