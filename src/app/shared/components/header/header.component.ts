1700afe6b4b55eccef64f85f6c1bb94f079f2b3b
Armando Zenil <szenil.11@gmail.com>
2020-07-30 16:05:47 -0500

    Uso de Pipe async para manejar un Observable

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart/cart.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,12 @@ import { CartService } from 'src/app/core/services/cart/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  totalItemsCart: number;
+  totalItemsCart$: Observable<number>;
 
   constructor(private cartService: CartService) {
-    this.cartService.cart$.subscribe(response => {
-      console.log(response);
-      this.totalItemsCart = response.length;
-    });
+    this.totalItemsCart$ = this.cartService.cart$.pipe(
+      map(productos => productos.length)
+    );
   }
 
   ngOnInit(): void {
